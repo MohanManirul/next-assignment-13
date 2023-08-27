16d5b2d63f5acf1b95d4fdf1e7951c4f5cb26132
MohanManirul <fiforeg@gmail.com>
2023-08-27 23:15:04 +0600

    headers

--- app/api/product/new/route.js
+++ app/api/product/new/route.js
@@ -5,58 +5,37 @@ import {headers} from "next/headers";
 //get request
 
 export async function GET(req,res){
-  const {searchParams} =  new URL(req.url);
-  let id = searchParams.get('id');
-  let name = searchParams.get('name');
-  let city = searchParams.get('city');
-    return NextResponse.json({
-        id : id,
-        name : name,
-        city : city
-    });
+   
 }
 
 
 //post request
 
 export async function POST(req,res){
-   const reqBody =  await req.json();
+    const headerList = headers();
+    const apiKey = headerList.get('api-key');
     return NextResponse.json({
-        id : reqBody['id'],
-        name : reqBody['name'],
-        city : reqBody['city']
-    })
+        msg : apiKey
+    });
 }
 
 
 //put request
 
 export async function PUT(req,res){
-    let Data = await req.formData(); // this is not a json object , its a  key value pair
-    let name = Data.get('name');
-    let city = Data.get('city');
-    let postCode = Data.get('postCode');
-    return NextResponse.json({
-        name : name,
-        city : city,
-        postCode : postCode,
-    });
+   
 }
 
 
 //patch request
 
 export async function PATCH(){
-    return NextResponse.json({
-        msg : "I'm PATCH Response"
-    });
+   
 }
 
 
 //delete request
 
 export async function DELETE(){
-    return NextResponse.json({
-        msg : "I'm DELETE Response"
-    });
+   
 }
