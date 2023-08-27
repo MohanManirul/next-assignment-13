491b3d047ef6f44ed20042b4a17f7e944abc345e
MohanManirul <fiforeg@gmail.com>
2023-08-27 21:33:43 +0600

    first commit

---
+++ app/page.js
@@ -0,0 +1,50 @@
+"use client"
+
+import { useEffect, useState } from 'react';
+import { getPosts } from './lib/getAllPost';
+
+
+export default function Home() {
+    const [posts , setPosts] = useState([]);
+    useEffect(()=>{
+
+            let data = getPosts().then((res)=>{
+                setPosts(res)
+            })
+
+
+    },[])
+
+
+  return (
+    <>
+            <div className="row">
+                <p>test section one</p>
+            </div>
+            <div className="row">
+                <p>test section two</p>
+            </div>
+        <div className="row">
+            <b className="text-center">Blog List</b>
+                {
+                    posts?.map((item,index)=>{
+                        return(
+                            <div key={index} className="card cardPadding col-md-4">
+                                
+                                <figure><img className="imgWidth"  src={item['img']} /></figure> 
+                                <div className="card-body">
+                                    <h2 className="card-title">{item.title}</h2>
+                                    <p>{item['short']}</p>
+                                </div> 
+                                                       
+                            </div> 
+                        )
+                    }) 
+                }
+            </div> 
+
+            
+    </>
+        
+  );
+}
