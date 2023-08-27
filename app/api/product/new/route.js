f6aa2741e747858d39f85c2916cacd14e7978015
MohanManirul <fiforeg@gmail.com>
2023-08-27 22:37:19 +0600

    before header configuration

--- app/api/product/new/route.js
+++ app/api/product/new/route.js
@@ -1,30 +1,45 @@
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
-
+import {headers} from "next/headers";
 
 //get request
 
 export async function GET(req,res){
+  const {searchParams} =  new URL(req.url);
+  let id = searchParams.get('id');
+  let name = searchParams.get('name');
+  let city = searchParams.get('city');
     return NextResponse.json({
-        msg : "I'm GET Response"
+        id : id,
+        name : name,
+        city : city
     });
 }
 
 
 //post request
 
-export async function POST(){
+export async function POST(req,res){
+   const reqBody =  await req.json();
     return NextResponse.json({
-        msg : "I'm POST Response"
-    });
+        id : reqBody['id'],
+        name : reqBody['name'],
+        city : reqBody['city']
+    })
 }
 
 
 //put request
 
-export async function PUT(){
+export async function PUT(req,res){
+    let Data = await req.formData(); // this is not a json object , its a  key value pair
+    let name = Data.get('name');
+    let city = Data.get('city');
+    let postCode = Data.get('postCode');
     return NextResponse.json({
-        msg : "I'm PUT Response"
+        name : name,
+        city : city,
+        postCode : postCode,
     });
 }
 
